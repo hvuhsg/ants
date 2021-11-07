bcc248a20b494b5795a3b2ad50a641156efc770f
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-11-07 15:08:25 +0200

    send telegram message via bot on failed ping (via 'notify' job)

--- examples/monitor/monitor_node.py
+++ examples/monitor/monitor_node.py
@@ -2,9 +2,10 @@ import os
 from typing import List
 from time import sleep
 from random import choice
-
+from datetime import datetime
 
 from loguru import logger
+from notifiers import get_notifier
 from ants import BaseNode, Job, State
 from ants.communications import SocketCommunication
 
@@ -12,6 +13,8 @@ from config import Config
 
 
 HEARTBEAT_INTERVAL = 30
+TELEGRAM_CHAT_ID = '-1001574719922'
+TELEGRAM_BOT_TOKEN = '2125627071:AAEoqEXo_r-unFGbHPCDXhJ6gBOk9xC9Xxc'
 
 
 class Node(BaseNode):
@@ -19,6 +22,7 @@ class Node(BaseNode):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.completed_jobs_set = set()
+        self.failed_pings = []
 
     def completed_jobs(self, done_jobs: List[Job]):
         for job in done_jobs:
@@ -30,6 +34,9 @@ class Node(BaseNode):
         for i in range(2):
             random_peer = choice(list(self.communication.peers))
             jobs.append(Job(name='ping', payload={'ip': random_peer[0]}))
+        for ping_data in self.failed_pings:
+            jobs.append(Job(name='notify', payload={'ip': ping_data['ip'], 'at': ping_data['date']}))
+        self.failed_pings.clear()
         return jobs  # Add job to ping peer ip
 
     def assign_to_jobs(self, pending_jobs: List[Job]):
@@ -46,14 +53,28 @@ class Node(BaseNode):
                     # ping_result = any(ping(target=job.payload['ip'], timeout=5, count=2))
                 except Exception:
                     job.set_result({'res': False, 'ip': job.payload['ip']})
+                    self.failed_pings.append({'ip': job.payload['ip'], 'date': datetime.utcnow().isoformat()})
                 else:
                     job.set_result({'res': ping_result, 'ip': job.payload['ip']})
+            elif job.name == 'notify':
+                telegram_notifier = get_notifier('telegram')
+                response = telegram_notifier.notify(
+                    message=f'🟥🟥 {job.payload["ip"]} ware down at {job.payload["at"]} 🟥🟥',
+                    chat_id=TELEGRAM_CHAT_ID,
+                    token=TELEGRAM_BOT_TOKEN,
+                )
+                job.set_result({'status': response.status, 'provider': response.provider})
 
 
 def run():
     node = Node(
         initial_config=Config(heartbeat_interval=HEARTBEAT_INTERVAL, max_assigned_jobs=1),
-        communication=SocketCommunication(host=f'0.0.0.0', port=34687, pull_interval=12),
+        communication=SocketCommunication(
+            host=f'0.0.0.0',
+            port=34687,
+            pull_interval=12,
+            bootstrap_nodes=[('5.183.9.78', 34687)]
+        ),
         initial_state=State(),
     )
     node.start()
