7d31aa1b886d8e469e44da3abaa881bc53a6fcd9
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-11-09 17:09:42 +0200

    add created_at field for message

--- ants/message.py
+++ ants/message.py
@@ -9,10 +9,13 @@ def sigmoid(x: float) -> float:
 
 
 class Message:
-    def __init__(self, sender: int, payload: dict, expiration_date: datetime):
+    def __init__(self, sender: int, payload: dict, expiration_date: datetime, created_at: datetime = None):
+        if created_at is None:
+            created_at = datetime.now()
         self.sender = sender
         self.payload = payload
         self.expiration_date = expiration_date
+        self.created_at = created_at
 
     @property
     def is_expired(self) -> bool:
@@ -27,13 +30,14 @@ class Message:
 
     @property
     def vector(self):
-        return sigmoid(self.expiration_date.timestamp())
+        return sigmoid(self.created_at.timestamp())
 
     def to_dict(self) -> dict:
         return {
             "sender": self.sender,
             "payload": self.payload,
             "expiration_date": self.expiration_date.isoformat(),
+            "created_at": self.created_at.isoformat(),
         }
 
     @classmethod
@@ -42,10 +46,11 @@ class Message:
             sender=dict_["sender"],
             payload=dict_["payload"],
             expiration_date=datetime.fromisoformat(dict_["expiration_date"]),
+            created_at=datetime.fromisoformat(dict_["created_at"]),
         )
 
     def __gt__(self, other):
-        return self.expiration_date > other.expiration_date
+        return self.vector > other.vector
 
     def __eq__(self, other):
-        return self.expiration_date == other.expiration_date
+        return self.vector == other.vector
